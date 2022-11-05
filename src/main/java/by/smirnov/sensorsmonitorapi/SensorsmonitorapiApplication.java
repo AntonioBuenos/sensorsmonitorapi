7f782adf94590056696b81735b80650e44b39415
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-05 17:59:50 +0300

    Initial commit

---
+++ src/main/java/by/smirnov/sensorsmonitorapi/SensorsmonitorapiApplication.java
@@ -0,0 +1,13 @@
+package by.smirnov.sensorsmonitorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsmonitorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsmonitorapiApplication.class, args);
+	}
+
+}
