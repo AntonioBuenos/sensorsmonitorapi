dcf14bb580d3aed2d16741fed320818748a2bed1
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-11 13:40:24 +0300

    Config done.

--- api/src/main/java/by/smirnov/SensorsmonitorapiApplication.java
+++ api/src/main/java/by/smirnov/SensorsmonitorapiApplication.java
@@ -1,9 +1,12 @@
-package by.smirnov.sensorsmonitorapi;
+package by.smirnov;
 
+import by.smirnov.config.PersistenceProvidersConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "by.smirnov")
+@Import({PersistenceProvidersConfiguration.class})
 public class SensorsmonitorapiApplication {
 
 	public static void main(String[] args) {
--- src/test/java/by/smirnov/SensorsmonitorapiApplicationTests.java
+++ src/test/java/by/smirnov/SensorsmonitorapiApplicationTests.java
@@ -1,4 +1,4 @@
-package by.smirnov.sensorsmonitorapi;
+package by.smirnov;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
