7661d427698beabaa439e1baf3f378f40c86ce28
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-15 16:30:42 +0300

    SensorConverter added.

---
+++ api/src/main/java/by/smirnov/dto/converter/SensorConverter.java
@@ -0,0 +1,47 @@
+package by.smirnov.dto.converter;
+
+import by.smirnov.domain.Sensor;
+import by.smirnov.domain.Type;
+import by.smirnov.dto.sensors.SensorRequest;
+import by.smirnov.dto.sensors.SensorResponse;
+import by.smirnov.service.SensorService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class SensorConverter {
+
+    private final ModelMapper modelMapper;
+    private final SensorService service;
+
+    public Sensor convert(SensorRequest request) {
+        Sensor created = modelMapper.map(request, Sensor.class);
+        created.setCreationDate(Timestamp.valueOf(LocalDateTime.now()));
+        created.setIsDeleted(false);
+
+        return created;
+    }
+
+    public Sensor convert(SensorRequest request, Long id) {
+        Sensor old = service.findById(id);
+        old.setModificationDate(Timestamp.valueOf(LocalDateTime.now()));
+        old.setName(request.getName());
+        old.setModel(request.getModel());
+        old.setRangeFrom(request.getRangeFrom());
+        old.setRangeTo(request.getRangeTo());
+        old.setType(Type.valueOf(request.getType()));
+        old.setLocation(request.getLocation());
+        old.setDescription(request.getDescription());
+        return old;
+    }
+
+    public SensorResponse convert(Sensor guitar) {
+        return modelMapper.map(guitar, SensorResponse.class);
+    }
+
+}
