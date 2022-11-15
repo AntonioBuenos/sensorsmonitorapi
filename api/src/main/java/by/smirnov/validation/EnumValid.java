ecd369789ccb9e4989ca87f99b030cff57df5b32
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-15 16:06:52 +0300

    Enum validation annotation added.

---
+++ api/src/main/java/by/smirnov/validation/EnumValid.java
@@ -0,0 +1,24 @@
+package by.smirnov.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = {EnumValueValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumValid {
+
+    String message() default "Invalid value. This is not permitted.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<? extends java.lang.Enum<?>> enumClass();
+}
---
+++ api/src/main/java/by/smirnov/validation/EnumValueValidator.java
@@ -0,0 +1,35 @@
+package by.smirnov.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValueValidator implements ConstraintValidator<EnumValid, String> {
+
+    private EnumValid annotation;
+
+    @Override
+    public void initialize(EnumValid annotation) {
+        this.annotation = annotation;
+    }
+
+    @Override
+    public boolean isValid(String valueForValidation, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result = false;
+
+        if (valueForValidation == null)
+            return true;
+
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (valueForValidation.equals(((java.lang.Enum<?>) enumValue).name())) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
